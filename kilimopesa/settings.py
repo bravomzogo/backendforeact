98a05fc9d057c0a43d33f59f7abaabe927246cfb
bravolambo <bravomzogo@gmail.com>
2025-07-19 00:58:04 +0300

    c

--- kilimopesa/settings.py
+++ kilimopesa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%j^32)0h+tog%ml3fly2d!3+pxztzl(dh-wy%qn#=1u9jamk89'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
